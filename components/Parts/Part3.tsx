a3302d7be419ff2498926bf94e66a1ad0134cb7d
Mark Maged <markmaged399@gmail.com>
2025-02-08 13:31:24 +0200

    add text editor

--- components/Parts/Part3.tsx
+++ components/Parts/Part3.tsx
@@ -1,24 +1,12 @@
 'use client';
 
-import { fetchUsers } from '@/api/Users';
-import { useEffect, useState } from 'react';
-import UserForm from '../forms/validation/UserForm';
-import { UsersResponse } from '@/types/User';
+import { useState } from 'react';
 import { getTranslation } from '@/i18n';
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 const Part3 = () => {
     const { t } = getTranslation();
-    const [isMounted, setIsMounted] = useState(false);
-    const [users, setUsers] = useState<UsersResponse>();
-    useEffect(() => {
-        const getUsers = async () => {
-            const data = await fetchUsers();
-            setUsers(data);
-        };
-        getUsers()
-        setIsMounted(true);
-    }, []);
-
+    const [value1, setValue1] = useState("");
     return (
         <>
             <div>
@@ -29,36 +17,9 @@ const Part3 = () => {
                     </div>
                 </div>
                 <div className="pt-5">
-                    <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+                    <div className="grid grid-cols-1 ">
                         <div className="h-full w-full">
-                            <UserForm />
-                        </div>
-                        <div className="h-full w-full">
-                            <div className="mb-5 flex items-center justify-between">
-                                <h5 className="text-lg font-semibold" style={{ color: '#6D5CBC' }}>{t('results')}</h5>
-                            </div>
-                            <div className="table-responsive" style={{ border: '1px solid #E5E5E5' }}>
-                                <table className="panel">
-                                    <thead>
-                                        <tr className="border-b-0">
-                                            <th>{t('first_name')}</th>
-                                            <th>{t('last_name')}</th>
-                                            <th>{t('phone')}</th>
-                                            <th>{t('email')}</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {users && users.length > 0 && users.map((user) => (
-                                            <tr key={user.id} className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
-                                                <td>{user.FirstName}</td>
-                                                <td>{user.LastName}</td>
-                                                <td>{user.Phone}</td>
-                                                <td>{user.Email}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
+                        <ReactQuill theme="snow" value={value1} onChange={setValue1} />
                         </div>
                     </div>
                 </div>
